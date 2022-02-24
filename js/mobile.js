6d92e24b2cce98ba5cb0716f796a20711d06a283
Ana Vrînceanu <anamariavrinceanu05@gmail.com>
2022-02-25 01:58:11 +0200

    Add files via upload

---
+++ js/mobile.js
@@ -0,0 +1,97 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const menuCloseProv = doc.querySelector(".closeprov");
+const menuCloseStiinta = doc.querySelector(".closestiinta");
+const menuCloseArmata = doc.querySelector(".closearmata");
+const menuCloseCase = doc.querySelector(".closecase");
+const menuCloseOrigini = doc.querySelector(".closeorigini");
+const menuCloseTemplu = doc.querySelector(".closetemplu");
+const menuCloseMumii = doc.querySelector(".closemumii");
+const menuCloseZei = doc.querySelector(".closezei");
+const menuCloseHicsosi = doc.querySelector(".closehicsosi");
+const menuCloseNef = doc.querySelector(".closenef");
+const menuCloseKarnak = doc.querySelector (".closekarnak");
+const menuClosePiramida = doc.querySelector (".closepiramida");
+const menuCloseRosetta = doc.querySelector (".closerosetta");
+const menuCloseScris = doc.querySelector (".closescris");
+const menuCloseTtk = doc.querySelector (".closettk");
+const menuCloseGalerie = doc.querySelector (".closegalerie");
+const menuCloseBibl = doc.querySelector (".closebibl");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseProv.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseStiinta.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseArmata.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseCase.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseOrigini.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseTemplu.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseMumii.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseZei.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseHicsosi.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseNef.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseKarnak.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuClosePiramida.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseRosetta.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseScris.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseTtk.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseGalerie.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+menuCloseBibl.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
